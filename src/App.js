f5959fd2e439cbcb9f70e0467b549f926f0bd00f
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-09 14:34:08 -0500

    refactored products

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 	};
 
 	return (
-	<ProductContext.Provider>
+	<ProductContext.Provider value={{ products, addItem }}>
 		<div className="App">
 			
 			<Navigation cart={cart} />
@@ -26,12 +26,7 @@ function App() {
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+			    component={Products}
 			/>
 
 			<Route
