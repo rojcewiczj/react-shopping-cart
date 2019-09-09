dbe3cf733315ef5bd633308f27be0bd18f74f4a4
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-09 14:55:29 -0500

    refactored app.js  to use CartContext

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from "./contexts/ProductContext";
-
+import CartContext from "./contexts/CartContext"
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -18,9 +18,10 @@ function App() {
 
 	return (
 	<ProductContext.Provider value={{ products, addItem }}>
+		<CartContext.Provider value={{cart}}>
 		<div className="App">
 			
-			<Navigation cart={cart} />
+			<Navigation />
 
 			{/* Routes */}
 			<Route
@@ -31,9 +32,10 @@ function App() {
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				component={ShoppingCart}
 			/>
 		</div>
+		</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
